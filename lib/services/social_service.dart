705f50e37c829f75a890e77164cb9220bf0b95d6
Pedro Henrique <phthedevx@gmail.com>
2025-10-05 12:40:28 -0300

    feat(social): adiciona método para enviar pedido de amizade no SocialService

--- lib/services/social_service.dart
+++ lib/services/social_service.dart
@@ -4,6 +4,24 @@ import 'package:erank_app/core/constants/api_constants.dart';
 import 'package:erank_app/services/auth_storage.dart';
 
 class SocialService {
+  static Future<bool> sendFriendRequest(int receiverId) async {
+    final token = await AuthStorage.getToken();
+    if (token == null) return false;
+
+    final response = await http.post(
+      Uri.parse('${ApiConstants.baseUrl}/amizades'),
+      headers: {
+        'Authorization': token,
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'idUsuario2': receiverId,
+      }),
+    );
+
+    return response.statusCode == 201; // Created
+  }
+
   static Future<List<dynamic>> searchUsers(String nickname) async {
     final token = await AuthStorage.getToken();
     if (token == null || nickname.isEmpty) return [];
