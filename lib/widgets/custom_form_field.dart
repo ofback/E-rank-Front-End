b4bf3b64fa08fada71faab21803853cbd3b8648c
Pedro Henrique <phthedevx@gmail.com>
2025-10-05 07:11:30 -0300

    feat(widgets): cria CustomFormField reutilizável

---
+++ lib/widgets/custom_form_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+
+  const CustomFormField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.validator,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.inputFormatters,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Detecta se o fundo é escuro para adaptar o estilo.
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      inputFormatters: inputFormatters,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          color: isDarkMode ? Colors.white54 : Colors.grey.shade600,
+        ),
+        filled: true,
+        fillColor:
+            isDarkMode ? Colors.white.withOpacity(0.1) : Colors.grey.shade100,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Color(0xFF7F5AF0)),
+        ),
+        errorStyle: const TextStyle(color: Colors.redAccent),
+      ),
+      validator: validator,
+    );
+  }
+}
