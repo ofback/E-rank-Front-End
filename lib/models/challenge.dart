5e4d31149be9012a5478ad72c1bc1ac7e8587664
Pedro Henrique <phthedevx@gmail.com>
2025-11-19 17:28:38 -0300

    feat(rf23): implementation of friends challenges (backend full + front service)

---
+++ lib/models/challenge.dart
@@ -0,0 +1,23 @@
+// lib/models/challenge.dart
+class Challenge {
+  final int id;
+  final String desafianteNome;
+  final String status;
+  final String dataHora;
+
+  Challenge({
+    required this.id,
+    required this.desafianteNome,
+    required this.status,
+    required this.dataHora,
+  });
+
+  factory Challenge.fromJson(Map<String, dynamic> json) {
+    return Challenge(
+      id: json['id'] ?? 0,
+      desafianteNome: json['desafianteNome'] ?? 'Desconhecido',
+      status: json['status'] ?? 'P',
+      dataHora: json['dataHora'] ?? '',
+    );
+  }
+}
